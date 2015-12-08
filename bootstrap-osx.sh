f0302031df00808bed9ff5bd7ce920edfa70f38e
AZaugg <AZaugg@users.noreply.github.com>
2015-12-08 14:35:13 +1100

    initial commit
    Initial brain dump

---
+++ bootstrap-osx.sh
@@ -0,0 +1,21 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+brew update
+brew install git wget python
+
+
+curl -so ~/.bashrc https://raw.githubusercontent.com/AZaugg/my-dotfiles/master/dot-bashrc
+curl -so ~/.gitconfig https://raw.githubusercontent.com/AZaugg/my-dotfiles/master/dot-gitconfig
+curl -so ~/.vimrc https://raw.githubusercontent.com/AZaugg/my-dotfiles/master/dot-vimrc
+
+pip install --upgrade setuptools
+pip install --upgrade pip 
+pip install virtualenv requests
+
+brew cask install virtualbox vagrant google-chrome atom slack hipchat iterm2
+ 
+# iTerm2 - install
+ 
+# Install atom plugins
+apm install vim-mode language-puppet linter-puppet-lint linter-pylint
